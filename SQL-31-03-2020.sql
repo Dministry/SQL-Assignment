13666b7211a309b97f0ea0a9db9d2299c19f1793
Dministry <46707781+Dministry@users.noreply.github.com>
2020-03-31 15:36:05 +0100

    Add files via upload

---
+++ SQL-31-03-2020.sql
@@ -0,0 +1,6 @@
+SELECT * FROM sql_store.order_items;
+SELECT * FROM sql_store.order_items 
+WHERE order_id=6 AND unit_price * quantity > 30;
+SELECT * FROM sql_store.products;
+SELECT * FROM sql_store.products
+WHERE quantity_in_stock IN (49, 38, 72)
